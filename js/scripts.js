83390be8e4386d6cf19ac8e8db910730604d9dc7
Bart Webber, Joe Mckinney, Maria Thomas and Paul Guevarra <bart@>
2017-07-19 11:14:31 -0700

    add tempScore and turn over function for player 2

--- js/scripts.js
+++ js/scripts.js
@@ -25,18 +25,41 @@ $(document).ready(function(){
   } else {
     tempScore += roll;
   }
-
-
   $("span#tempScore").text(tempScore);
+  });
+
+  $("button#roll-button2").click(function(event){
+    event.preventDefault();
+
+  var roll = rollDice();
+  $("span#result2").text(roll);
+  if (roll === 1){
+    tempScore = 0;
+    if (roll === 1){
+      alert("1 Rolled..... turn over");
+      $("#player2panel").hide();
+      $("#player1panel").show();
+    }
+  } else {
+    tempScore += roll;
+  }
+  $("span#tempScore2").text(tempScore);
   });
 
   $("button#hold-button").click(function(event){
     player1 += tempScore;
     tempScore = 0;
-  $("span#player1Score").text(player1);
-  if (player1 >= 100){
-  alert("player1 WINS!!!");
-
-  }
+    $("span#player1Score").text(player1);
+    if (player1 >= 100){
+      alert("player1 WINS!!!");
+    }
+  });
+  $("button#hold-button2").click(function(event){
+    player2 += tempScore;
+    tempScore = 0;
+    $("span#player2Score").text(player2);
+    if (player2 >= 100){
+      alert("player2 WINS!!!");
+    }
   });
 });
